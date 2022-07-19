fb480662e5cdc18bc9d11ec1cf887de02f12c4c5
GustavoBozo <gustavinhobozo@gmail.com>
2022-07-19 17:53:35 -0300

    Primeiro commit

---
+++ pages/basicos/jsx1.js
@@ -0,0 +1,9 @@
+// JSX = Misturar HTML dentro de um código JavaScript
+
+export default function jsx1Diferente() {
+    return (
+        <div>
+            <h1>JSX #1</h1>
+        </div>
+    )
+}
