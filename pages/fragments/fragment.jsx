6c07c5c111adac61d0460bcf4c25c61579b0bfda
GustavoBozo <gustavinhobozo@gmail.com>
2022-07-20 17:59:09 -0300

    Fragmentos criados

---
+++ pages/fragments/fragment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function fragment() {
+    return (
+        <React.Fragment>
+            <h1>Testando</h1>
+            <h2>Fragmentoos</h2>
+        </React.Fragment>
+    )
+}   
+
+export default fragment
---
+++ pages/fragments/fragment2.jsx
@@ -0,0 +1,10 @@
+function fragment2() {
+    return (
+        <>
+            <h1>Testando Fragmentos</h1>
+            <h2>Novamente</h2>
+        </>
+    )
+}
+
+export default fragment2;
