92906b9ef5abc3a015fa463213de7bb2155b013c
GustavoBozo <gustavinhobozo@gmail.com>
2022-07-20 16:53:37 -0300

    Desafio Lista Span

--- pages/basicos/lista.jsx
+++ pages/basicos/lista.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 export default function lista1() {
+    const [valor, setValor] = useState('')
+
     return (
         <div>
             <h1>Exemplo de lista</h1>
-            {gerarLista().map((item) => {
+            <input type="text"
+            onInput={e => setValor(e.target.value)} value={valor}/> <br />
+            {gerarLista(valor).map((item) => {
                 return (
                     <strong key={item}>{item}, </strong>
                 )
@@ -11,9 +17,10 @@ export default function lista1() {
     )
 }
 
-function gerarLista() {
+function gerarLista(valor = 10) {
     const lista = []
-    for(let i = 0; i <= 20; i++){
+    
+    for(let i = 0; i <= valor; i++){
         const frase = i
         lista.push(frase)
     }
